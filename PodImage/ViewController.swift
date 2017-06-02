6fcb21cfcef3438e2d93483a6c5c5bfbfce4ad62
tipparti <tipparti@Nainas-MacBook-Pro.local>
2017-06-02 10:51:08 -0700

    Call func createUser()

--- PodImage/ViewController.swift
+++ PodImage/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createTable()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
